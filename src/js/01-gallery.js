89abbe56bdcfe9f304a2c9f3cdc7673ed5b05c3b
Vadym Starchenko <starvadim@ukr.net>
2022-02-07 12:53:37 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refGallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+}).join('');
+
+
+refGallery.insertAdjacentHTML('beforeend', markup);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightBoxOptions = {
+ captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+let lightbox = new SimpleLightbox('.gallery__item', lightBoxOptions);
