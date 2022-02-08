affed0a0cda15ccb6513e3de5ec767cd4d438740
Vadym Starchenko <starvadim@ukr.net>
2022-02-08 16:07:56 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,29 +6,26 @@ import throttle from 'lodash.throttle';
 // Инициализируй плеер в файле скрипта
 const player = new Player('vimeo-player');
 
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения
-// обновлялось в хранилище не чаще чем раз в секунду.
-
-const throttledFunction = throttle(updateTime, 1000);
-
-function updateTime(e) {
-  localStorage.setItem(VIDEO_CURRENT_TIME, e.seconds);
-}
-
-// Начни отслеживать событие timeupdate - обновление времени воспроизведения.
-
-player.on('timeupdate', throttledFunction);
-
 // Сохраняй время воспроизведения в локальное хранилище.
 // Пусть ключом для хранилища будет строка "videoplayer-current-time".
 
-const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+const onPlay = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения
+// обновлялось в хранилище не чаще чем раз в секунду.
+// Начни отслеживать событие timeupdate - обновление времени воспроизведения.
+
+player.on('timeupdate', throttle(onPlay, 1000));
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
 
 // При перезагрузке страницы воспользуйся методом setCurrentTime()
 // для того чтобы возобновить воспроизведение с сохраненной позиции.
 
-if (localStorage[VIDEO_CURRENT_TIME]) {
-  player.setCurrentTime(localStorage[VIDEO_CURRENT_TIME]);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
 }
 
 
